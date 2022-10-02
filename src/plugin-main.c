20cb2dfa8af1cb92204e22c557594453c868b72b
Leigh Oliver <leigh@leigholiver.com>
2022-10-02 13:58:24 +0100

    attempt rebuild via actions from obs-plugintemplate... 🙏

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,4 +1,28 @@
+/*
+Plugin Name
+Copyright (C) <Year> <Developer> <Email Address>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #include <obs-module.h>
+
+#include "plugin-macros.generated.h"
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
 #include <QAction>
 #include <QMainWindow>
 
@@ -10,13 +34,13 @@
 #include "Webhook.h"
 #include "forms/SettingsDialog.h"
 
-OBS_DECLARE_MODULE()
-
 SceneSwitcher* sw = nullptr;
 ScoreTracker* st = nullptr;
 Webhook* wh = nullptr;
 
 bool obs_module_load(void) {
+	blog(LOG_INFO, "plugin loaded successfully (version %s)",
+	     PLUGIN_VERSION);
 	obs_frontend_add_save_callback(LoadSaveHandler, nullptr);
 
 	//set up the settings dialog
@@ -43,6 +67,7 @@ bool obs_module_load(void) {
 }
 
 void obs_module_unload(void) {
+	blog(LOG_INFO, "plugin unloaded");
 	Config::free();
 	delete sw;
 	delete wh;
