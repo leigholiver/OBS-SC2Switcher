e587c18da9fa078a42e061df724256b768f2b25c
Leigh Oliver <leigh@leigholiver.com>
2022-10-06 22:42:15 +0100

    trying random things

--- src/log.h
+++ src/log.h
@@ -2,10 +2,11 @@
 #include <string>
 #include <obs-frontend-api.h>
 #include "Config.h"
+#define PLUGIN_NAME "@CMAKE_PROJECT_NAME@"
 
 static void s2log(std::string message) {
 	Config* cfg = Config::Current();
 	if(cfg->logging) {
-		blog(LOG_INFO, message.c_str());
+		blog(level, "[" PLUGIN_NAME "] " message.c_str());
 	}
 }
