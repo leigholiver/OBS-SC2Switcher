e852e3673170605afb0e021baa3e9923c84946dd
Leigh Oliver <leigh@leigholiver.com>
2022-10-06 23:03:07 +0100

    no logging

--- src/log.h
+++ src/log.h
@@ -6,6 +6,6 @@
 static void s2log(std::string message) {
 	Config* cfg = Config::Current();
 	if(cfg->logging) {
-		blog(LOG_INFO, "[" PLUGIN_NAME "] " message.c_str());
+		//blog(LOG_INFO, "[" PLUGIN_NAME "] " message.c_str());
 	}
 }
