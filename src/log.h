6784457c1817f930fef76ad031de3b356c44687a
Leigh Oliver <leigh@leigholiver.com>
2022-10-06 20:49:56 +0100

    how do c strings work

--- src/log.h
+++ src/log.h
@@ -6,6 +6,9 @@
 static void s2log(std::string message) {
 	Config* cfg = Config::Current();
 	if(cfg->logging) {
-		blog(LOG_INFO, sprintf("[sc2switcher] %s".c_str(), message.c_str()));
+		blog(
+			LOG_INFO,
+			sprintf("[sc2switcher] %s", message.c_str()).c_str()
+		);
 	}
 }
